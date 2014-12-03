414b45ac24323705968a66454aa542936e42ce9c
huston007 <ktyby007>
2014-12-03 22:21:15 +0300

    Fix ngRoute and others angular modules including

--- demo/myApp.js
+++ demo/myApp.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['dependency']);
+var myApp = angular.module('myApp', ['dependency', 'ngRoute']);
 
 myApp.run(function($document){
     $document[0].write('Hello from myApp. ');
